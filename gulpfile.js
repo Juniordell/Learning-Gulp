f3a0028aa23fe5f3fbb8b93186f59a47bc6c8a83
Nelson Jr <juniordell123@gmail.com>
2020-08-13 15:34:17 -0300

    Add the third task to minify the css

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,15 @@
 var gulp = require('gulp')
-
+var cleanCSS = require('gulp-clean-css')
 var sass = require('gulp-sass')
 
 gulp.task('default', function() {
     console.log('Hello Gulp!')
+})
+
+gulp.task('minify-css', function() {
+    return gulp.src('style/style.css')
+        .pipe(cleanCSS())
+        .pipe(gulp.dest('style'))
 })
 
 gulp.task('sass', function() {
