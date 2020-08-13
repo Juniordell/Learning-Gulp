a7ce01442f60d67fcc36483def926a4fa4b0b781
Nelson Jr <juniordell123@gmail.com>
2020-08-13 15:32:44 -0300

    Add the second task and running it to compile the sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp')
 
+var sass = require('gulp-sass')
+
 gulp.task('default', function() {
     console.log('Hello Gulp!')
 })
 
+gulp.task('sass', function() {
+    return gulp.src('src/style/style.scss')
+        .pipe(sass().on('Error', sass.logError))
+        .pipe(gulp.dest('style'))
+})
